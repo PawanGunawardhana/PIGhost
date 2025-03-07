ab5cc3a207e7d5de0c45f88139f100483808eeb9
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-07 23:45:33 +0530

    Define some Animations and put all components together

--- js/main.js
+++ js/main.js
@@ -2,5 +2,15 @@ const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
+   
   })
 );
+
+// function playSound(){
+//   let audio = new Audio("https://www.myinstants.com/media/sounds/vine-boom.mp3");
+//   setInterval(()=>{
+//     audio.play();
+
+//   }, Math.random()*10000+5000);
+// }
+// playSound();
